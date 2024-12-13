bc1e09e5a26d64a1d78ff1b42428ffe322269730
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-12-13 14:14:44 -0500

    flesh out app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { QuestionCard } from "./components/QuestionCard";
 
 function App() {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
     <div className="app">
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Begin Quiz
+      </button>
+      <p className="score">Score: </p>
+      <p>Loading questions...</p>
       <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
